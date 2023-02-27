e43a87811e8d694b97c0a191ed6dc5819a364aac
Decebal Demetrius Vissarion <36327813+demetriusvissarion@users.noreply.github.com>
2023-02-27 21:02:45 +0000

    moved error handling + notifications into slice

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import Products from "./components/Shop/Products";
 
 import { useDispatch, useSelector } from "react-redux";
 import { Fragment, useEffect } from "react";
-import { uiActions } from "./store/ui-slice";
 import Notification from "./components/UI/Notification";
+import { sendCartData } from "./store/cart";
 
 let isInitial = true;
 
@@ -16,46 +16,13 @@ function App() {
   const notification = useSelector((state) => state.ui.notification);
 
   useEffect(() => {
-    const sendCartData = async () => {
-      dispatch(
-        uiActions.showNotification({
-          status: "pending",
-          title: "Sending...",
-          message: "Sending cart data!",
-        })
-      );
-      const response = await fetch(
-        "https://redux-shop-3a34c-default-rtdb.europe-west1.firebasedatabase.app/cart.json",
-        { method: "PUT", body: JSON.stringify(cart) }
-      );
-
-      if (!response.ok) {
-        throw new Error("Sending cart data failed.");
-      }
-      dispatch(
-        uiActions.showNotification({
-          status: "success",
-          title: "Success!!!",
-          message: "Sent cart data successfully!",
-        })
-      );
-    };
-
     if (isInitial) {
       isInitial = false;
       return;
     }
 
-    sendCartData().catch((error) => {
-      dispatch(
-        uiActions.showNotification({
-          status: "error",
-          title: "Error!!!",
-          message: "Sent cart data failed!",
-        })
-      );
-    });
-  }, [cart, dispatch]); // 'PUT' overwrites existing data
+    dispatch(sendCartData(cart));
+  }, [cart, dispatch]);
 
   return (
     <Fragment>
