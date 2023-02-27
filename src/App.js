a033885f57e2a0ad0614eca23162a95b045f0a8a
Decebal Demetrius Vissarion <36327813+demetriusvissarion@users.noreply.github.com>
2023-02-27 15:15:37 +0000

    sending cart status to firebase

--- src/App.js
+++ src/App.js
@@ -3,9 +3,18 @@ import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
 
 import { useSelector } from "react-redux";
+import { useEffect } from "react";
 
 function App() {
   const showCart = useSelector((state) => state.myCart.showCart);
+  const cart = useSelector((state) => state.cart); // subscription
+
+  useEffect(() => {
+    fetch(
+      "https://redux-shop-3a34c-default-rtdb.europe-west1.firebasedatabase.app/cart.json",
+      { method: "PUT", body: JSON.stringify(cart) }
+    );
+  }, [cart]); // 'PUT' overwrites existing data
 
   return (
     <Layout>
--- src/components/Shop/ProductItem.js
+++ src/components/Shop/ProductItem.js
@@ -1,7 +1,7 @@
 import Card from "../UI/Card";
 import classes from "./ProductItem.module.css";
 
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { cartActions } from "../../store/cart";
 
 const ProductItem = (props) => {
